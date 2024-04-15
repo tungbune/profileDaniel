19b4926d341ca22b9749f74dff3756395926199b
tungbune <150694590+tungbune@users.noreply.github.com>
2024-04-16 00:58:00 +0700

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,9 +7,6 @@ import workImgFive from "./work/workImgFive.jpg"
 import workImgSix from "./work/workImgSix.jpg"
 import workImgSeven from "./work/workImgSeven.jpg"
 import workImgEight from "./work/workImgEight.jpg"
-import blogImgOne from "./blog/blogImgOne.jpg"
-import blogImgTwo from "./blog/blogImgTwo.jpg"
-import blogImgThree from "./blog/blogImgThree.jpg"
 
 export { 
     bannerImg,
@@ -21,7 +18,5 @@ export {
     workImgSix,
     workImgSeven,
     workImgEight,
-    blogImgOne,
-    blogImgTwo,
-    blogImgThree
+   
 };
