2f3225ae4b4f576600541e83a3dfe9454707e97d
tungbune <150694590+tungbune@users.noreply.github.com>
2024-04-16 11:58:29 +0700

    Update About.js

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -2,14 +2,14 @@ import React from "react";
 import Title from "../home/Title";
 import AboutMe from "./AboutMe";
 import FunFact from "./FunFact";
-import MyServices from "./MyServices";
+import EducationFoundation from "./EducationFoundation";
 
 const About = () => {
   return (
     <section id="about" className="w-full">
       <Title title="About" subTitle="Me" />
       <AboutMe />
-      <Title title="My" subTitle="Services" />
+      <Title title="Education" subTitle="Foundation" />
       <MyServices />
       <Title title="Fun" subTitle="Fact" />
       <FunFact />
